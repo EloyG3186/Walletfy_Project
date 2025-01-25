fa2444b59db4bf0c6c350d3948b4c545f8baed26
EloyG3186 <156388775+EloyG3186@users.noreply.github.com>
2025-01-24 22:27:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://dcortesnet.github.io/Reactjs-deploy-githubpages'
+  base: 'https://EloyG3186.github.io/Walletfy_Project'
 })
